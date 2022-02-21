197f15758f6b4c323e88485bceacfebeff7e47c7
Otto Winter <otto@otto-winter.com>
2022-02-21 23:41:53 +0100

    Switch brainfuck to bfc

--- cms/grading/languages/brainfuck.py
+++ cms/grading/languages/brainfuck.py
@@ -1,11 +1,11 @@
-from cms.grading import Language
+from cms.grading import CompiledLanguage
+from pathlib import Path
 
 
 __all__ = ["Brainfuck"]
 
 
-class Brainfuck(Language):
-
+class Brainfuck(CompiledLanguage):
     @property
     def name(self):
         return "Brainfuck"
@@ -15,21 +15,16 @@ class Brainfuck(Language):
         return [".bf"]
 
     @property
-    def executable_extension(self):
-        """See Language.executable_extension."""
-        return ".bf"
+    def object_extensions(self):
+        """See Language.source_extensions."""
+        return [".o"]
 
     def get_compilation_commands(self,
                                  source_filenames, executable_filename,
                                  for_evaluation=True):
-        if source_filenames[0] != executable_filename:
-            return [["/bin/cp", source_filenames[0], executable_filename]]
-        else:
-            # We need at least one command to collect execution stats.
-            return [["/bin/true"]]
-
-    def get_evaluation_commands(
-            self, executable_filename, main=None, args=None):
-        """See Language.get_evaluation_commands."""
-        args = args if args is not None else []
-        return [["/usr/bin/bf", executable_filename] + args]
+        """See Language.get_compilation_commands."""
+        fname = source_filenames[0]
+        return [
+            ["/usr/bin/bfc", fname],
+            ["/usr/bin/cp", Path(fname).with_suffix(""), executable_filename]
+        ]
