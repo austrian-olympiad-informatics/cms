c1a4a02912b61ce06390d1f6d38a784b53395575
Otto Winter <otto@otto-winter.com>
2022-02-22 11:40:09 +0100

    Revert "Switch brainfuck to bfc"
    This reverts commit 197f15758f6b4c323e88485bceacfebeff7e47c7.

--- cms/grading/languages/brainfuck.py
+++ cms/grading/languages/brainfuck.py
@@ -1,11 +1,11 @@
-from cms.grading import CompiledLanguage
-from pathlib import Path
+from cms.grading import Language
 
 
 __all__ = ["Brainfuck"]
 
 
-class Brainfuck(CompiledLanguage):
+class Brainfuck(Language):
+
     @property
     def name(self):
         return "Brainfuck"
@@ -15,16 +15,21 @@ class Brainfuck(CompiledLanguage):
         return [".bf"]
 
     @property
-    def object_extensions(self):
-        """See Language.source_extensions."""
-        return [".o"]
+    def executable_extension(self):
+        """See Language.executable_extension."""
+        return ".bf"
 
     def get_compilation_commands(self,
                                  source_filenames, executable_filename,
                                  for_evaluation=True):
-        """See Language.get_compilation_commands."""
-        fname = source_filenames[0]
-        return [
-            ["/usr/bin/bfc", fname],
-            ["/usr/bin/cp", Path(fname).with_suffix(""), executable_filename]
-        ]
+        if source_filenames[0] != executable_filename:
+            return [["/bin/cp", source_filenames[0], executable_filename]]
+        else:
+            # We need at least one command to collect execution stats.
+            return [["/bin/true"]]
+
+    def get_evaluation_commands(
+            self, executable_filename, main=None, args=None):
+        """See Language.get_evaluation_commands."""
+        args = args if args is not None else []
+        return [["/usr/bin/bf", executable_filename] + args]
