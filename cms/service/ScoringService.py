2a85b4c78695d9326a9baa8f878d24bc28ae326c
Otto Winter <otto@otto-winter.com>
2023-01-05 22:36:24 +0100

    Store subtask scoring results in DB for faster queries

--- cms/service/ScoringService.py
+++ cms/service/ScoringService.py
@@ -28,7 +28,7 @@
 import logging
 
 from cms import ServiceCoord, config
-from cms.db import SessionGen, Submission, Dataset, get_submission_results
+from cms.db import SessionGen, Submission, Dataset, get_submission_results, SubtaskScore
 from cms.io import Executor, TriggeredService, rpc_method
 from cmscommon.datetime import make_datetime
 from .scoringoperations import ScoringOperation, choose_meme, get_operations
@@ -99,6 +99,34 @@ class ScoringExecutor(Executor):
                 submission_result.public_score_details, \
                 submission_result.ranking_score_details = \
                 score_type.compute_score(submission_result)
+
+            submission_result.subtask_scores = []
+            if (
+                submission_result.score_details is not None and
+                # did not compile
+                not (submission_result.score_details == [] and submission_result.score == 0)
+            ):
+                st_datas = None
+                try:
+                    st_datas = {
+                        st["idx"]: st["max_score"] * st["score_fraction"]
+                        for st in submission_result.score_details
+                    }
+                except (KeyError, ValueError):
+                    pass
+
+                if not st_datas:
+                    # Task's score type is not group, assume a single subtask.
+                    st_datas = {1: submission_result.score}
+
+                for idx, score in st_datas.items():
+                    st_score = SubtaskScore(
+                        submission_id=submission_result.submission_id,
+                        dataset_id=submission_result.dataset_id,
+                        subtask_idx=idx,
+                        score=score,
+                    )
+                    submission_result.subtask_scores.append(st_score)
 
             submission_result.meme = choose_meme(session, submission_result)
 
