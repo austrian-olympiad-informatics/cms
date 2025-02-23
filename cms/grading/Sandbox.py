f40490f018d327bbdc363170b5a1b849b54dfb84
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2025-02-23 20:20:05 +0100

    replace /dev/shm by our own tmpfs to prevent communication

--- cms/grading/Sandbox.py
+++ cms/grading/Sandbox.py
@@ -1087,6 +1087,7 @@ class IsolateSandbox(SandboxBase):
                 f'--dir=/={config.chroot_base_image}',
                 '--dir=proc=proc:fs',
                 '--dir=dev:dev',
+                '--dir=/dev/shm:tmpfs:fs:rw', # replace /dev/shm by our own tmpfs to prevent communication
                 '--dir=box=./box:rw',
             ]
         if self.chdir is not None:
